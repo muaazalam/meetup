42af48bcbc80dacaef919cb6392c92c8249ba682
muaazalam <alammuaazz@gmail.com>
2024-06-03 09:54:02 -0700

    error

--- src/App.js
+++ src/App.js
@@ -2,18 +2,17 @@ import { Routes, Route } from "react-router-dom";
 import AllMeetupsPage from "./pages/AllMeetups";
 import NewMeetupsPage from "./pages/NewMeetups";
 import FavouritesPage from "./pages/Favourites";
-import MainNavigation from "./components/layout/MainNavigation";
+import Layout from "./components/layout/Layout";
 
 
 function App(){
-  return <div>
-    <MainNavigation></MainNavigation>
+  return <Layout>
     <Routes>
     <Route path='/' element={<AllMeetupsPage />} />
     <Route path='/new-meetup' element={<NewMeetupsPage />} />
     <Route path='/favourites' element={<FavouritesPage />} />
     </Routes>
-  </div>;
+  </Layout>;
 }
 
 export default App;
